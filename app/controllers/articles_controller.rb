44f0d748f4dadbb0723a2aaa3980b4ea45a14910
heatblst <ashishkumar07865apk@gmail.com>
2021-04-20 18:16:47 +0530

    Pagination completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index
-        @articles = Article.all
+
+        @articles = Article.paginate(page: params[:page], per_page: 5)
+        
     end
     def new
         @article = Article.new
@@ -9,6 +11,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:success] = "Articles was successfully created"
             redirect_to article_path(@article)
